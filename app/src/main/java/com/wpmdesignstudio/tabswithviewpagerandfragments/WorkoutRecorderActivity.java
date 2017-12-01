d98a2af9969967136944944d36bbfde552fe4949
Shamar Morrison <shamar_morrison2000@yahoo.conm>
2017-12-01 12:05:25 -0500

    Implementing swiping tabs with viewpager in WorkoutRecorder Activity

--- app/src/main/java/com/wpmdesignstudio/tabswithviewpagerandfragments/WorkoutRecorderActivity.java
+++ app/src/main/java/com/wpmdesignstudio/tabswithviewpagerandfragments/WorkoutRecorderActivity.java
@@ -1,17 +1,29 @@
 package com.wpmdesignstudio.tabswithviewpagerandfragments;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class WorkoutRecorderActivity extends AppCompatActivity {
 
-    String title;
+    Toolbar toolbar;
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_recorder);
-        title = "Name";
-        ((WorkoutRecorderActivity)this).getSupportActionBar().setTitle(title);
+        initXML();
+    }
+
+    private void initXML() {
+        tabLayout = (TabLayout) findViewById(R.id.workout_recorder_activity_tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.workout_recorder_activity_viewpager);
+        toolbar = (Toolbar) findViewById(R.id.workout_recorder_activity_toolbar);
+        setSupportActionBar(toolbar);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
