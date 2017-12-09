de5e6465fd3b035630630b295b1bc9883634afc5
shamar <shamar_morrison2000@yahoo.com>
2017-12-09 02:18:28 -0500

    Wed, Dec. 6, 2017 - 11:04 P.M. - Laptop
    TabLayout not showing tabs, fix it.

--- app/src/main/java/com/wpmdesignstudio/tabswithviewpagerandfragments/TrackFragment.java
+++ app/src/main/java/com/wpmdesignstudio/tabswithviewpagerandfragments/TrackFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,6 +14,9 @@ import android.widget.TextView;
  * A simple {@link Fragment} subclass.
  */
 public class TrackFragment extends Fragment {
+
+    private EditText weightEditText;
+    private EditText repsEditText;
 
 
     public TrackFragment() {
@@ -24,7 +28,9 @@ public class TrackFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_track_layout, container, false);
-        ListView listView = (ListView) view.findViewById(R.id.list);
+        weightEditText = (EditText) view.findViewById(R.id.weight_editText);
+        repsEditText = (EditText) view.findViewById(R.id.reps_editTEXT);
+        ListView listView = (ListView) view.findViewById(android.R.id.list);
         return view;
     }
 
